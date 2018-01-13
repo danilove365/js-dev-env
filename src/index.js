e16a6c8bc40bf53f8f1a2d9eaa6ff4429d55ab7c
Danielle <danilove365@gmail.com>
2018-01-12 21:48:55 -0600

    third base

---
+++ src/index.js
@@ -0,0 +1,5 @@
+import './index.css';
+
+const today = new Date();
+const currentTime = `${today.getHours()}:${today.getMinutes()}:${today.getSeconds()}`;
+console.log(`The current time is ${currentTime}`);
