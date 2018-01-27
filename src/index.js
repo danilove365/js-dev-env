978ead695079afb404ea5851d89e826d5edd629e
Danielle <danilove365@gmail.com>
2018-01-27 15:41:13 -0600

    Fourth base

--- src/index.js
+++ src/index.js
@@ -2,4 +2,5 @@ import './index.css';
 
 const today = new Date();
 const currentTime = `${today.getHours()}:${today.getMinutes()}:${today.getSeconds()}`;
+debugger; //eslint-disable-line no-debugger
 console.log(`The current time is ${currentTime}`);
